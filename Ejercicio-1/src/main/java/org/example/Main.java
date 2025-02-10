2f3981a55eda528e91d2859acbeff6703c87349b
DAM <2061784@alu.murciaeduca.es>
2025-02-10 17:38:28 +0100

    ejercicio1 clase

--- Ejercicio-1/src/main/java/org/example/Main.java
+++ Ejercicio-1/src/main/java/org/example/Main.java
@@ -1,7 +1,50 @@
 package org.example;
 
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        try{
+            Properties prop = new Properties();
+            prop.load(new FileInputStream("src/main/resources/propierties.properties"));
+
+
+            FTPClient clienteFTP = new FTPClient();
+            clienteFTP.connect(prop.getProperty("hostname"), 21);
+
+            int respuesta=clienteFTP.getReplyCode();
+            if(FTPReply.isPositiveCompletion(respuesta)){
+
+
+                clienteFTP.login(prop.getProperty("username"), prop.getProperty("password"));
+
+                clienteFTP.enterLocalPassiveMode();
+
+                System.out.println("Carpetas disponibles en Servidor:");
+
+                String[] nombreCarpeta = clienteFTP.listNames();
+
+                for (String s : nombreCarpeta) {
+                    System.out.println(s);
+                }
+
+                clienteFTP.disconnect();
+                System.out.println("Descarga finalizada correctamente");
+            }else{
+                clienteFTP.disconnect();
+                System.err.println("FTP ha rechazado la conexión esblecida");
+                System.exit(1);
+            }
+
+        }catch(IOException e){
+            System.err.println("Error al leer el archivo : "+e.getMessage());
+        }
+
     }
 }
