97d4dde8edbed4d84fb51505ef2e0f9a53dfdceb
DAM <2061784@alu.murciaeduca.es>
2025-02-10 19:12:58 +0100

    punto 2 a medias

--- Ejercicio-1/src/main/java/org/example/Main.java
+++ Ejercicio-1/src/main/java/org/example/Main.java
@@ -17,20 +17,56 @@ public class Main {
         String opcion;
         do {
             mostrarMenu();
-            opcion = sc.nextLine();
+            opcion =sc.nextLine();
+            elegirOpcion(opcion);
         }while (!Objects.equals(opcion, "6"));
 
+    }
+    public static void mostrarMenu(){
+
+        System.out.println("MENU");
+        System.out.println("1. Mostrar directorio actual");
+        System.out.println("2. Entrar directorio");
+        System.out.println("3. Subir al directorio padre");
+        System.out.println("4. Subir fichero: ");
+        System.out.println("5. Borrar un fichero: ");
+        System.out.println("6. Salir");
+    }
+    public static void elegirOpcion(String opcion){
+
+        switch(opcion){
+            case "1":
+                mostrarDirectorio();
+                break;
+            case "2":
+                entrarDirectorio();
+                break;
+            case "3":
+                subirDirectorioPadre();
+                break;
+            case "4":
+                subirFichero();
+                break;
+            case "5":
+                borrarFichero();
+                break;
+            case "6":
+                break;
+            default:
+                System.out.println("Opcion no valida");
+        }
+    }
+    public static void mostrarDirectorio(){
         try{
             Properties prop = new Properties();
             prop.load(new FileInputStream("src/main/resources/propierties.properties"));
 
-
             FTPClient clienteFTP = new FTPClient();
+
             clienteFTP.connect(prop.getProperty("hostname"), Integer.parseInt(prop.getProperty("puerto")));
 
             int respuesta=clienteFTP.getReplyCode();
             if(FTPReply.isPositiveCompletion(respuesta)){
-
 
                 clienteFTP.login(prop.getProperty("usuario"), prop.getProperty("password"));
 
@@ -43,9 +79,7 @@ public class Main {
                 for (FTPFile s : nombreCarpeta) {
                     System.out.println(s);
                 }
-
                 clienteFTP.disconnect();
-                System.out.println("Descarga finalizada correctamente");
             }else{
                 clienteFTP.disconnect();
                 System.err.println("FTP ha rechazado la conexión esblecida");
@@ -55,19 +89,51 @@ public class Main {
         }catch(IOException e){
             System.err.println("Error al leer el archivo : "+e.getMessage());
         }
+    }
+    public static void entrarDirectorio(){
+        try{
+            Properties prop = new Properties();
+            prop.load(new FileInputStream("src/main/resources/propierties.properties"));
+
+
+            FTPClient clienteFTP = new FTPClient();
+            clienteFTP.connect(prop.getProperty("hostname"), Integer.parseInt(prop.getProperty("puerto")));
+
+            int respuesta=clienteFTP.getReplyCode();
+            if(FTPReply.isPositiveCompletion(respuesta)){
+
+                clienteFTP.login(prop.getProperty("usuario"), prop.getProperty("password"));
+
+                System.out.println("Introduce el directorio al que quieres acceder:");
+                String directorio = sc.nextLine();
+                boolean hecho = clienteFTP.changeWorkingDirectory(directorio);
+                if (hecho) {
+                    System.out.println("Se ha entrado al directorio");
+                } else {
+                    System.out.println("No se ha entrado al directorio");
+                }
+
+
+                clienteFTP.disconnect();
+            }else{
+                clienteFTP.disconnect();
+                System.err.println("FTP ha rechazado la conexión esblecida");
+                System.exit(1);
+            }
+
+
+        }catch(IOException e){
+            System.err.println("Error al leer el archivo : "+e.getMessage());
+        }
 
     }
+    public static void subirDirectorioPadre(){
 
-    public static void mostrarMenu(){
+    }
+    public static void subirFichero(){
 
-        System.out.println("MENU");
-        System.out.println("1. Mostrar directorio actual");
-        System.out.println("2. Entrar directorio");
-        System.out.println("3. Subir al directorio padre");
-        System.out.println("4. Subir chero: ");
-        System.out.println("5. Borrar un chero: ");
-        System.out.println("6. Salir");
-
+    }
+    public static void borrarFichero(){
 
     }
 }
